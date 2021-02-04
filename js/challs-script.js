485e8168d905207ed9e71e990c51f4b9f6a76d9b
Bucheron <bucheron@pop-os.localdomain>
2021-02-04 19:39:23 +0100

    En construction

--- js/challs-script.js
+++ js/challs-script.js
@@ -47,19 +47,30 @@ function setupChalls() {
 	$challDiv.children()[1].innerHTML = list[0].title;
 
 	var $modalDiv = $('.modal');
-        $modalDiv.attr('id', list[0].name);
+  $modalDiv.attr('id', list[0].name);
 
 	var modal = document.getElementById(list[0].name).childNodes; // modal
-        var tmp = modal[1].childNodes; // modal-dialog
-        tmp = tmp[1].childNodes; // modal-content
-        var header = tmp[1].childNodes; // modal-header
-        var body = tmp[3].childNodes; // modal-body
-        var footer = tmp[5].childNodes; // modal-footer
+  var tmp = modal[1].childNodes; // modal-dialog
+  tmp = tmp[1].childNodes; // modal-content
+  var header = tmp[1].childNodes; // modal-header
+  var body = tmp[3].childNodes; // modal-body
+  var footer = tmp[5].childNodes; // modal-footer
 	var $modalDiv = $('.modal');
 	$modalDiv.attr('id', list[0].name);
 
 	header[1].innerHTML = list[0].title;
 	body[1].innerHTML = list[0].desc;
+  var fileElem = body[3];
+  if(list[0].file != "") {
+    fileElem.classList.add('btn-white');
+    fileElem.setAttribute('href', 'challenge-files/' + cat + '/' + list[0].name + '/' + list[0].file);
+    fileElem.setAttribute('target', '_blank');
+    fileElem.setAttribute('download', list[0].file);
+    fileElem.innerHTML = list[0].file;
+    fileElem.style.display = "inline-block";
+  } else {
+    fileElem.style.display = "none";
+  }
   //footer[1].attr('onclick', "getKey('challenge-files/" + cat + "/" + list[0].name + "/key.json');")
 
 	//console.log($challDiv);
@@ -96,6 +107,17 @@ function setupChalls() {
 
 		header[1].innerHTML = list[i].title;
 		body[1].innerHTML = list[i].desc;
+    var fileElem = body[3];
+    if(list[i].file != "") {
+      fileElem.classList.add('btn-white');
+      fileElem.setAttribute('href', 'challenge-files/' + cat + '/' + list[i].name + '/' + list[i].file);
+      fileElem.setAttribute('target', '_blank');
+      fileElem.setAttribute('download', list[i].file);
+      fileElem.innerHTML = list[i].file;
+      fileElem.style.display = "inline-block";
+    } else {
+      fileElem.style.display = "none";
+    }
     //footer[1].attr('onclick', "getKey('challenge-files/" + cat + "/" + list[i].name + "/key.json');")
 	}
 }
