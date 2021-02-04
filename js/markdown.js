5edfd591fac2a643c7fb0127c4063dc66cabd005
Bucheron <bucheron@pop-os.localdomain>
2021-02-04 17:45:53 +0100

    En construction

--- js/markdown.js
+++ js/markdown.js
@@ -133,4 +133,4 @@ const file = urlParams.get('file');
 
 document.getElementById('title-cat').innerHTML = (cat.charAt(0).toUpperCase() + cat.slice(1))
 
-showMDpage('https://raw.githubusercontent.com/ClimbingFromBottom/ClimbingFromBottom/main/learning-md/' + cat + '/' + file + '.md');
+showMDpage('https://raw.githubusercontent.com/ClimbingFromBottom/ClimbingFromBottom.github.io/main/learning-md/' + cat + '/' + file + '.md');
